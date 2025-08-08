ff4f98e50b8d2f033bd7f68d5f8f1848e9a0a235
Marcel2201020012 <marcelaja314@gmail.com>
2025-08-08 14:47:51 +0700

    fixing vercel not found

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,8 @@ import tailwindcss from "@tailwindcss/vite"
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.VITE_BASE_PATH || "/Agro_Resto_Client"
+  base: process.env.VITE_BASE_PATH || "/Agro_Resto_Client",
+  server: {
+    historyApiFallback: true
+  }
 })
