b1afe644317b08aca9b0fcefb69efe73d545909e
Marcel2201020012 <marcelaja314@gmail.com>
2025-08-13 21:36:17 +0700

    add payment gateway

--- api/createTransaction.js
+++ api/createTransaction.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
     return res.status(405).json({ error: "Method not allowed" });
   }
 
-  const { order, amount, name } = req.body;
+  const { order, amount, name, email } = req.body;
 
   try {
     let snap = new midtransClient.Snap({
@@ -24,10 +24,7 @@ export default async function handler(req, res) {
       },
     };
 
-    const transaction = await snap.createTransaction(parameter).then((transaction) => {
-      let transactionToken = transaction.token;
-      console.log('transactionToken:', transactionToken);
-    });
+    const transaction = await snap.createTransaction(parameter);
     res.status(200).json({ token: transaction.token });
   } catch (err) {
     res.status(500).json({ error: err.message });
