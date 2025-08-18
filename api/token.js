0e59fd38b66603779ea3413ea760d94f9109bc85
Marcel2201020012 <marcelaja314@gmail.com>
2025-08-18 21:32:21 +0700

    removing token

--- api/token.js
+++
@@ -1,8 +0,0 @@
-export default function handler(req, res) {
-  const { token } = req.query;
-  if (token === process.env.SECRET_TOKEN) {
-    res.status(200).json({ access: true });
-  } else {
-    res.status(403).json({ access: false });
-  }
-}
